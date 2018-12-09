55ae8cec7d782316ebde44742819c9d518b8b141
DESKTOP-7IETLFJ\임다혜 <devldh@naver.com>
2018-12-10 03:22:15 +0900

    mysql 연결 및 조회하기

--- HelloExpress/routes/index.js
+++ HelloExpress/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+    user: 'root',
+    password: '1111',
+    database: 'node_db'
+})
+
+router.get('/', function(req, res, next) {
+    client.query('SELECT * FROM member;', function(err, result, fields) {
+        if(err) console.log('쿼리문에 오류가 있습니다.');
+        else res.json(result);
+    });
+});
+
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+*/
 module.exports = router;
